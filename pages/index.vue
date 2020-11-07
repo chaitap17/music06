50e087d5eaa8d2f641a382309474ae6a0e73af27
chaitap17 <68525409+chaitap17@users.noreply.github.com>
2020-11-07 20:01:32 +0700

    new

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="10">
+      <div class="text-center">
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline"> Welcome to A PLAY MUSIC. </v-card-title>
+        <v-card-text>
+          <p>IF YOU WANT TO VIEW PROFILE CLICK CONTiUNUE</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="success" nuxt to="/inspire">
+            Continue
+            <v-icon>mdi-cursor-default-click</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+export default {
+  components: {
+    VuetifyLogo,
+  },
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://i.imgur.com/5XCJuJR.png',
+        },
+        {
+          src: 'https://i.imgur.com/w2ybn4S.png',
+        },
+        {
+          src: 'https://i.imgur.com/nGoNQg0.png',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style></style>
